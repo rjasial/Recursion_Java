01377554c11c7da5cc3cbd1f406a976a70fdce2f
rjasial <116119951+rjasial@users.noreply.github.com>
2026-02-19 17:28:12 -0800

    Updated

--- recursionJava.java
+++ recursionJava.java
@@ -7,7 +7,11 @@ public class recursionJava {
 
         //calFact(5, 1);
 
-        fibo(0, 1, 8);
+        //fibo(0, 1, 8);
+        
+        System.out.println(calPower(2, 5));
+        System.out.println(power(2, 5));
+        towerOfHanoi(3, "S", "H", "D");
     }
 
     //
@@ -58,7 +62,62 @@ public class recursionJava {
         fibo(b, b+a, n-1);
     }
 
-    //
+    // X^n stack height = n
+
+    public static int calPower(int x, int n) {
+        
+        if(n==0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+
+        int xPownm1 = calPower(x, n-1);
+        int xPown = x * xPownm1;
+        return xPown;
+    }
+
+    // // X^n stack height = logn
+
+    public static int power(int x, int n) {
+        
+        if(n==0){
+            return 1;
+        }
+
+        if(n % 2 == 0){
+            return power(x, n/2) * power(x, n/2);
+        }else{
+            return x * power(x, n/2) * power(x, n/2);
+        }
+
+        
+    }
+
+    //TOwer of hanoi
+
+    public static void towerOfHanoi(int n, String Source, String Helper, String Destination) {
+        
+        if( n == 1){
+            System.out.println("Disk " + n + " had been tranferred from " + Source + " to " + Destination );
+            return;
+        }
+
+        towerOfHanoi(n-1, Source, Destination, Helper);
+        System.out.println("Disk " + n + " had been tranferred from " + Source + " to " + Destination);
+        towerOfHanoi(n-1, Helper, Source, Destination);
+    }
+
+
+
+
+
+
+
+    
+
+
 
 
 
